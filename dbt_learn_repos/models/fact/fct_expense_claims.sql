b75aa122c567c4d83c8c166fdce38ceb9076b69e
saikrishnasnflk2 <beyonddatatech01@gmail.com>
2025-11-16 20:02:15 +0530

    comm

---
+++ dbt_learn_repos/models/fact/fct_expense_claims.sql
@@ -0,0 +1,20 @@
+with claims as (
+    select * from {{ ref('stg_expense_claims') }}
+),
+
+employees as (
+    select * from {{ ref('employee_master') }}
+)
+
+select
+    c.claim_id,
+    c.employee_id,
+    e.employee_name,
+    e.department,
+    c.claim_date,
+    c.amount,
+    c.status,
+    c.description
+from claims c
+left join employees e
+   on c.employee_id = e.employee_id
